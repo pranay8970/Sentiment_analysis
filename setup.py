f9e7f22e44aeb6d1af9d39deb9b9dc66f18a35c8
pranay8970 <148626247+pranay8970@users.noreply.github.com>
2025-11-16 22:31:16 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'pranay8970',
+    author_email= 'pranayreddy8970@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
